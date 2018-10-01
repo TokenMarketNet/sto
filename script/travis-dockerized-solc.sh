1ccbc9141b309d5d5a960da180163daae22e9fe0
Mikko Ohtamaa <mikko@opensourcehacker.com>
2018-10-01 13:33:17 +0200

    Another stab to get docker working under Travis

---
+++ script/travis-dockerized-solc.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# This is a faux solc stub that runs a dockerized solc to run under Travis
+#
+
+VERSION=$SOLC_VERSION
+
+ME=`dirname "$0"`
+
+if [ -z "$SOLC_VERSION" ] ; then
+  echo "You need to give SOLC_VERSION environment variable"
+  exit 1
+fi
+
+# note: I still do not know how to create different path maps for solc. TokenMarket contracts refer to Zeppelin contracts.
+# However regardless where you run solc it assumes Zeppelin contracts are within the current working directory
+# under zeppelin folder. This is a workaround if you want to invoke populus compile not under this path,
+# but some other path and get Populus testrpc fixture working.
+# This is mainly a workaround to get our internal test suite to complete. Before this could be maintained
+# using a symbolic link, but Docker volume mounts ignore symbolic links.
+docker run -p 127.0.0.1:80:4567 -i -a stdin -a stdout -a stderr -v `pwd`:`pwd` -v $ME/zeppelin:`pwd`/zeppelin -w `pwd` ethereum/solc:$VERSION "$@"
