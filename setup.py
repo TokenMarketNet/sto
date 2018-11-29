aafc5d2e345d3c868571105a1542d2635d07184d
Mikko Ohtamaa <mikko@opensourcehacker.com>
2018-11-29 22:48:28 +0100

    More bumpversion woes

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ dev_requirements = [
 
 setup(
     name='sto',
-    version='version='version='0.1.0-alpha.4',',',
+    version='0.1.0-alpha.4',
     description="Security token management tool",
     long_description=readme + '\n\n',
     author="TokenMarket Ltd.",
