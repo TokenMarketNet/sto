46172d2decefe2138a21bb30b7ea8795fe33ef4f
Mikko Ohtamaa <mikko@opensourcehacker.com>
2019-01-02 13:52:08 +0100

    Make sure MANIFEST.in lists are more sane

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ dev_requirements = [
 
 setup(
     name='sto',
-    version='0.3.2'
+    version='0.3.2',
     description="Security token management tool",
     long_description=readme + '\n\n',
     author="TokenMarket Ltd.",
