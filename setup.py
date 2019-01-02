c4ba249b4be803cb2890a9f3cb271f51a94ef390
Mikko Ohtamaa <mikko@opensourcehacker.com>
2019-01-02 15:02:37 +0100

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ dev_requirements = [
 
 setup(
     name='sto',
-    version='version=0.3.3',
+    version='version=version='0.3.4',',
     description="Security token management tool",
     long_description=readme + '\n\n',
     author="TokenMarket Ltd.",
