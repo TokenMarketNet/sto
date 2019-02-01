c4fe2505d808e25830b044de42a3d3d085cff3b0
voith <voithjm1@gmail.com>
2019-02-02 01:11:14 +0530

    fixed constructor abi encoding

--- sto/ethereum/utils.py
+++ sto/ethereum/utils.py
@@ -11,7 +11,14 @@ from web3.contract import Contract
 from web3.utils.abi import get_constructor_abi, merge_args_and_kwargs
 from web3.utils.events import get_event_data
 from web3.utils.filters import construct_event_filter_params
-from eth_utils import keccak, to_checksum_address, to_bytes, is_hex_address, is_checksum_address
+from eth_utils import (
+    keccak,
+    to_checksum_address,
+    to_bytes,
+    is_hex_address,
+    is_checksum_address,
+    to_hex
+)
 from web3.utils.contracts import encode_abi
 
 from sto.cli.main import is_ethereum_network
@@ -104,6 +111,10 @@ def get_constructor_arguments(contract: Contract, args: Optional[list]=None, kwa
 
     # return contract._encode_constructor_data(args=args, kwargs=kwargs)
     constructor_abi = get_constructor_abi(contract.abi)
+
+    # constructor_abi can be none in case of libraries
+    if constructor_abi is None:
+        return to_hex(contract.bytecode)
 
     if args is not None:
         return contract._encode_abi(constructor_abi, args)[2:]  # No 0x
@@ -214,13 +225,19 @@ def deploy_contract(config, contract_name, constructor_args=()):
     check_good_private_key(config.ethereum_private_key)
 
     abi = get_abi(config.ethereum_abi_file)
+
     for dependency in abi[contract_name]['ordered_full_dependencies']:
-        deploy_contract(config, dependency)
+        deploy_contract(config, dependency, None)
 
     abi[contract_name]['bytecode'] = _link_bytecode(
         config.dbsession,
         abi[contract_name]['bytecode'],
         abi[contract_name]['linkrefs']
+    )
+    abi[contract_name]['bytecode_runtime'] = _link_bytecode(
+        config.dbsession,
+        abi[contract_name]['bytecode_runtime'],
+        abi[contract_name]['linkrefs_runtime'],
     )
 
 
