2f4a0f6fa24be20c45a1cc4e86da8f20554a2510
voith <voithjm1@gmail.com>
2019-03-14 15:34:00 +0530

    clean up

--- sto/models/broadcastaccount.py
+++ sto/models/broadcastaccount.py
@@ -60,9 +60,6 @@ class _PreparedTransaction(TimeStampedBaseModel):
     #: Precalculated transaction id
     txid = sa.Column(sa.String(256), nullable=True)
 
-    #: tx_type can `ether` or `token`
-    tx_type = sa.Column(sa.String(256), nullable=True)
-
     #: Value transferred in Ethereum transaction
     # value = sa.Column(sa.Numeric(60, 20), nullable=False, default=0)
 
